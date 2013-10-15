d460f662cd10a88661466b1e7be8115a37b98456
thejasvi <tvenka1@pride.hofstra.edu>
2013-10-14 21:47:53 -0400

    changes to check the time for first argument

--- stat2.c
+++ stat2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
-
+#include <time.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[])
@@ -12,11 +12,15 @@ main(int argc, char *argv[])
 
 int i;
 struct stat buf;
+time_t tempfmtime;
+time_t fmtime;
 char *ptr;
+char *filename;
+char *tempfilename;
  
        if (argc != 4) {
-               fprintf(stderr, "Usage: %s <pathname> <pathname> <pathname>\n");
-               exit(1);
+               fprintf(stderr, "Usage: stat <pathname> <pathname> <pathname>\n");
+               exit(0);
           }
 
        for (i = 1; i < argc; i++) 
@@ -24,7 +28,13 @@ char *ptr;
          if (stat(argv[i], &buf) < 0) 
            {
              perror("stat error");
+             exit(1);
            } 
+            if(i==1) {
+                   fmtime= sb.st_mtime;
+                   filename = argv[i];
+             }
+                 
            printf("Last file access:         %s", ctime(&buf.st_atime));
            printf("Last file modification:   %s", ctime(&buf.st_mtime)); 
          }

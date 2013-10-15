13153e44d08f1f2c52ad654b5288b6d9dd15040e
thejasvi <tvenka1@pride.hofstra.edu>
2013-10-14 22:00:00 -0400

    changes made to check other times in the argumen arguments to compare with the first pathname time

--- stat2.c
+++ stat2.c
@@ -25,18 +25,28 @@ char *tempfilename;
 
        for (i = 1; i < argc; i++) 
         {
-         if (stat(argv[i], &buf) < 0) 
+         if (stat(argv[i], &buf) == -1) 
            {
              perror("stat error");
              exit(1);
            } 
+         printf("file Name is %s with modified time %s \n", argv[i], ctime(&buf.st_mtime);
             if(i==1) {
-                   fmtime= sb.st_mtime;
+                   fmtime= buf.st_mtime;
                    filename = argv[i];
              }
-                 
-           printf("Last file access:         %s", ctime(&buf.st_atime));
-           printf("Last file modification:   %s", ctime(&buf.st_mtime)); 
+            printf(" filename %s" , filename);
+            printf(" time %s". fmtime);
+                }
+ 
+          else
+            {
+                 tempfilename = argv[i];
+                 tempfmtime= buf.st_mtime;
+             }              
+
+
+ 
          }
      exit(0);
 }
